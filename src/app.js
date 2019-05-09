841d9f6ad8845c2684868aed747e42ad621b97cc
Ike Njoku <ikeenjoku@gmail.com>
2019-05-09 10:15:07 +0100

    add mongo connection config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
-
 import modules from './modules';
 
 dotenv.config();
@@ -15,6 +15,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // connect to database
+if (process.env.NODE_ENV === 'production') {
+  // get url from mlab
+  mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+} else {
+  mongoose.connect('mongodb://localhost:27017/sms-api', { useNewUrlParser: true });
+  const db = mongoose.connection;
+  // mongo errors
+  db.on('error', console.error.bind(console, 'connection error: '));
+  db.once('open', () => { console.log('DB connection established'); });
+}
 
 modules(app);
 // handle 404 error
@@ -25,12 +35,10 @@ app.use((req, res, next) => {
 });
 
 // Catch all errors
-app.use((err, req, res) => {
-  return res.status(err.status || 500).json({
-    status: err.status,
-    message: err.message,
-    stack: err.stack,
-  });
-});
+app.use((err, req, res) => res.status(err.status || 500).json({
+  status: err.status,
+  message: err.message,
+  stack: err.stack,
+}));
 
 export default app;
