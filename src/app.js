61858f2b440dcfb496c1928d6f35a3f263cb5987
Ike Njoku <ikeenjoku@gmail.com>
2019-05-09 09:02:28 +0100

    setup basic express server

--- src/app.js
+++ src/app.js
@@ -4,5 +4,34 @@ import bodyParser from 'body-parser';
 import logger from 'morgan';
 import dotenv from 'dotenv';
 
-
+dotenv.config();
 const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// connect to database
+app.use((req, res, next) => {
+  return res.status(200).json({
+    message: 'It is working',
+  });
+});
+// handle 404 error
+app.use((req, res, next) => {
+  const pageNotFound = new Error('There is no page here');
+  pageNotFound.status = 404;
+  next(pageNotFound);
+});
+
+// Catch all errors
+app.use((err, req, res) => {
+  return res.status(err.status || 500).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+export default app;
