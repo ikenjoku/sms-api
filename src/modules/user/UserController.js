b06476ac078b9ef6c28b645c2bb2141c0c9ad14c
Ike Njoku <ikeenjoku@gmail.com>
2019-05-09 13:39:42 +0100

    add sign up route

--- src/modules/user/UserController.js
+++ src/modules/user/UserController.js
@@ -1,15 +1,19 @@
 import User from '../../data/models/user';
 
 class UserController {
-  static async signUpUser(req, res) {
-    const {
-      username, email, password,
-    } = req.body;
-
-    return res.status(201).json({
-      success: true,
-      message: 'Create a user',
-    });
+  static async signUpUser(req, res, next) {
+    const { username, phone } = req.body;
+    if (!username || !phone) {
+      const err = new Error('All fields are required to signup');
+      err.status = 400;
+      next(err);
+    } else {
+      const newUser = new User(req.body);
+      newUser.save((err, user) => {
+        if (err) return next(err);
+        return res.status(201).json(user);
+      });
+    }
   }
 
   static async loginUser(req, res) {
