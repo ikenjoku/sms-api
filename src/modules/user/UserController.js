da3687264d34db12b458d2276be86cfc7a69c11a
Ike Njoku <ikeenjoku@gmail.com>
2019-05-10 15:06:09 +0100

    add authentication routes

--- src/modules/user/UserController.js
+++ src/modules/user/UserController.js
@@ -1,26 +1,69 @@
+import bcrypt from 'bcrypt';
+import tokenizer from '../../helpers/tokenizer';
+
 import User from '../../data/models/user';
 
 class UserController {
-  static async signUpUser(req, res, next) {
-    const { username, phone } = req.body;
-    if (!username || !phone) {
-      const err = new Error('All fields are required to signup');
-      err.status = 400;
-      next(err);
-    } else {
-      const newUser = new User(req.body);
-      newUser.save((err, user) => {
-        if (err) return next(err);
-        return res.status(201).json(user);
+  static async registerUser(req, res) {
+    const {
+      username,
+      phone,
+      email,
+      password,
+    } = req.body;
+
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const userParams = {
+        username,
+        phone,
+        email,
+        password: hashedPassword,
+      };
+      const newUser = await User.create(userParams);
+      const token = await tokenizer.createToken(newUser);
+      return res.status(201).json({
+        message: 'Successfully registered',
+        token,
+        user: {
+          id: newUser._id,
+          username: newUser.username,
+          email: newUser.email,
+          phone: newUser.phone,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: 'Unable to register at this time',
+        error,
       });
     }
   }
 
   static async loginUser(req, res) {
-    return res.status(201).json({
-      success: true,
-      message: 'login a user',
-    });
+    try {
+      const { user } = req;
+      delete user.password;
+      const token = await tokenizer.createToken(user);
+      return res.status(200).json({
+        message: `Welcome back ${user.username}`,
+        token,
+        user: {
+          id: user._id,
+          username: user.username,
+          phone: user.phone,
+          email: user.email,
+        },
+      });
+    } catch (error) {
+      return res.status(500).json({
+        success: false,
+        message: 'Unable to perfom this action',
+        error,
+      });
+    }
   }
 }
+
 export default UserController;
